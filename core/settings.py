4c235b96dac358ef26fbe299bb05de0ae551d0c7
yosef <yosfemyayu@gmail.com>
2024-11-29 11:10:57 +0300

    installing cors header

--- core/settings.py
+++ core/settings.py
@@ -36,11 +36,13 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_nested",
     "drf_spectacular",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -215,6 +217,10 @@ UNFOLD = {
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    # "lhost",
+]
 # SPECTACULAR_SETTINGS = {
 #     'TITLE': 'Your Project API',
 #     'DESCRIPTION': 'Your project description',
