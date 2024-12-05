15fa0d51eec799f6bcf4240e21b74c2714a2b89f
yosef <yosfemyayu@gmail.com>
2024-12-05 17:23:19 +0300

    post status filter

--- core/settings/common.py
+++ core/settings/common.py
@@ -204,8 +204,8 @@ UNFOLD = {
 
 REST_FRAMEWORK = {
     # "COERCE_DECIMAL_TO_STRING": False,
-    # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    # "PAGE_SIZE": 50,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
